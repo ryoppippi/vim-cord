6452df8072cf4edf08c96991e70114e472b74202
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-02 22:23:13 +0100

    feat: implement dummy status

--- denops/vim-cord/main.ts
+++ denops/vim-cord/main.ts
@@ -1,27 +1,45 @@
 import { Denops } from "jsr:@denops/std@7.1.1";
-import { g } from "jsr:@denops/std@7.1.1/variable";
-import * as helper from "jsr:@denops/std@7.1.1/helper";
-import * as fn from "jsr:@denops/std@7.1.1/function";
 import { assert, is } from "jsr:@core/unknownutil";
 import { effect, ref } from "npm:@vue/reactivity@3.4.31";
+import { Client } from "npm:@xhayper/discord-rpc@1.2.0";
+import pRetry from "npm:p-retry@6.2.0";
 
-import { type Client, useClient } from "./client.ts";
 import { CLIENT_IDS } from "./constants.ts";
 
+/** Denops instance */
 const denopsClient = ref<Denops>();
-const discordClient = ref<Client>();
+
+/** Discord RPC client */
+let discordClient: Client;
+/** Discord RPC client ready state */
+const discordClientReady = ref(false);
+
+effect(() => {
+  console.log("change detected");
+  if (discordClientReady.value) {
+    const host = denopsClient.value?.meta.host;
+    discordClient?.user?.setActivity({
+      state: `${host ?? "?"} is running`,
+      details: "Pain and Suffering",
+      startTimestamp: Date.now(),
+    });
+  }
+});
 
 async function init(denops: Denops) {
   denopsClient.value = denops;
+
   const clientId = CLIENT_IDS[denops.meta.host];
-  discordClient.value = await useClient(clientId);
+  discordClient = new Client({ clientId });
+  discordClient?.on("ready", () => discordClientReady.value = true);
+  await pRetry(() => discordClient?.login(), { retries: 5 });
 }
 
 export function main(denops: Denops) {
   denops.dispatcher = {
     /** Initialize the plugin */
-    init: async () => {
-      await init(denops);
+    init: () => {
+      init(denops);
     },
   };
 }
